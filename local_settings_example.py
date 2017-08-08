4fb64328da758d1001f99e3994b52243ad119343
Ekevoo <ekevoo@users.noreply.github.com>
2017-08-07 22:03:50 -0400

    Inital commit

---
+++ local_settings_example.py
@@ -0,0 +1,21 @@
+"""
+Local Settings for a twitter key.
+"""
+
+# Credentials
+MY_CONSUMER_KEY = 'Your Twitter API Consumer Key'
+MY_CONSUMER_SECRET = 'Your Consumer Secret Key'
+MY_ACCESS_TOKEN_KEY = 'Your Twitter API Access Token Key'
+MY_ACCESS_TOKEN_SECRET = 'Your Access Token Secret'
+
+# The name of the account you're counting days.
+TWEET_ACCOUNT = ''
+
+# Target date (use YYYY-MM-DD notation), this is day 0.
+TARGET_DATE = '2017-08-25'
+
+# Formats! Use {} to represent the count.
+# Max length: 20 chrs'aoeuidhtnspyfgcrlqjk'
+BEFORE_TARGET_DATE = 'My name ({} to go)'
+DURING_TARGET_DATE = 'My name'
+AFTER__TARGET_DATE = 'My name'
